6174d3ffaf217da512e20cd0b117fde513e97a22
23n2! <zenzile.sithole@gmail.com>
2019-04-18 11:29:35 +0200

    Updated min support to dart 2.0.0 and Added analysis options file

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,43 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_card_io/flutter_card_io.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   @override
-  _MyAppState createState() => new _MyAppState();
+  _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  Map<String, dynamic> _data = {};
-
-  @override
-  initState() {
-    super.initState();
-  }
+  Map<String, dynamic> _data = <String, dynamic>{};
 
   // Platform messages are asynchronous, so we initialize in an async method.
-  _scanCard() async {
+  Future<void> _scanCard() async {
     Map<String, dynamic> details;
     // Platform messages may fail, so we use a try/catch PlatformException.
     try {
-      details = await FlutterCardIo.scanCard({
-        "requireExpiry": true,
-        "scanExpiry": true,
-        "requireCVV": true,
-        "requirePostalCode": true,
-        "restrictPostalCodeToNumericOnly": true,
-        "requireCardHolderName": true,
-        "scanInstructions": "Hola! Fit the card within the box",
+      details = await FlutterCardIo.scanCard(<String, dynamic>{
+        'requireExpiry': true,
+        'scanExpiry': true,
+        'requireCVV': true,
+        'requirePostalCode': true,
+        'restrictPostalCodeToNumericOnly': true,
+        'requireCardHolderName': true,
+        'scanInstructions': 'Hola! Fit the card within the box',
       });
 
     } on PlatformException {
-      print("Failed");
+      print('Failed');
       return;
     }
 
     if (details == null) {
-      print("Canceled");
+      print('Canceled');
       return;
     }
 
@@ -51,23 +46,23 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('CardIO sample app'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('CardIO sample app'),
         ),
-        body: new Column(
+        body: Column(
           children: <Widget>[
-            new Column(
+            Column(
               children: _data.keys.map((String key) {
-                return new Text(_data[key].toString());
+                return Text(_data[key].toString());
               }).toList(),
             ),
-            new Container(
-              margin: new EdgeInsets.all(30.0),
-              child: new Center(
-                child: new RaisedButton(
-                  child: new Text("Scan"),
+            Container(
+              margin: const EdgeInsets.all(30.0),
+              child: Center(
+                child: RaisedButton(
+                  child: const Text('Scan'),
                   onPressed: _scanCard,
                 ),
               ),
--- lib/flutter_card_io.dart
+++ lib/flutter_card_io.dart
@@ -3,11 +3,10 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class FlutterCardIo {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_card_io');
+  static const MethodChannel _channel = MethodChannel('flutter_card_io');
 
   static Future<dynamic> scanCard(Map<String, dynamic> args) {
-    return _channel.invokeMethod('scanCard', args);
+    return _channel.invokeMethod<dynamic>('scanCard', args);
   }
 
 }
